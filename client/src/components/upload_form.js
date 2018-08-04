335499a6cbf39a1bc8a8ca9f570a049a682b8eda
Bret <Burhooo@gmail.com>
2018-08-04 14:44:58 -0500

    added readFile() to UploadForm, preparing payload

--- client/src/components/upload_form.js
+++ client/src/components/upload_form.js
@@ -3,6 +3,18 @@ import Dropzone from 'react-dropzone'
 
 export default class UploadForm extends React.Component {
 
+    readFile = (files) => {
+        if(files && files[0]) {
+            let formPayload = new FormData();
+            formPayload.append('uploaded_image', files[0]);
+            this.sendImageToController(formPayload);
+        }
+    }
+
+    sendImageToController = (formPayload) => {
+
+    }
+
     render() {
         return(
             <Dropzone onDrop={this.readFile}>
