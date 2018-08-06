097d92dd34838ddf8f7a90035139116c9bbc0a17
Bret <Burhooo@gmail.com>
2018-08-06 07:05:03 -0500

    Adding to upload form

--- client/src/components/upload_form.js
+++ client/src/components/upload_form.js
@@ -6,38 +6,59 @@ export default class UploadForm extends React.Component {
         super()
 
         this.state = {
-            uploads: ''
+            uploads: '',
+            files: [],
+            title: ''
         }
     }
 
-    readFile = (files) => {
-        if(files && files[0]) {
-            let formPayload = new FormData();
-            formPayload.append('uploaded_image', files[0]);
-            this.sendImageToController(formPayload);
-        }
-    }
-
-    sendImageToController = (formPayload) => {
-        fetch(`/pieces/new`, {
-            credentials: 'same-origin',
-            headers: {},
-            method: 'POST',
-            body: formPayload
-          })
-          //todo error handling?
-          .then(response => response.json())
-          .then(imageFromController => {
-          this.setState({uploads: this.state.uploads.concat(imageFromController)})
+    handleTextChange = (event) => {
+        this.setState({
+            title: event.target.value
         })
-        console.log(this.state)
     }
+
+    handleDrop = (files) => {
+        console.log('hello?')
+        this.setState({
+            files
+        })
+        console.log('hi', this.state)
+    }
+
+    // readFile = (files) => {
+    //     if(files && files[0]) {
+    //         let formPayload = new FormData();
+    //         formPayload.append('uploaded_image', files[0]);
+    //         this.sendImageToController(formPayload);
+    //     }
+    // }
+
+    // sendImageToController = (formPayload) => {
+    //     fetch(`/pieces/new`, {
+    //         credentials: 'same-origin',
+    //         headers: {},
+    //         method: 'POST',
+    //         body: formPayload
+    //       })
+    //       //todo error handling?
+    //       .then(response => response.json())
+    //       .then(imageFromController => {
+    //       this.setState({uploads: this.state.uploads.concat(imageFromController)})
+    //     })
+    //     console.log(this.state)
+    // }
 
     render() {
         return(
-            <Dropzone onDrop={this.readFile}>
-                <button>Upload a new image</button>
-            </Dropzone>
+                <div>
+                    <h3>Upload a new image</h3>
+                    Title: <input type='text' name='title' value={this.state.title} onChange={this.handleTextChange} />
+                    <input type='submit' />
+                    <Dropzone onDrop={this.handleDrop}>
+                        <button>Upload a new image</button>
+                    </Dropzone>
+                </div>
         )
     }
 }
