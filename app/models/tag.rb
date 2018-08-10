03eb9b13238f8a766184eec58487a3f01603d335
Bret <Burhooo@gmail.com>
2018-08-10 06:24:20 -0500

    tags/pieces rails associations are set up

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
     has_many :tags_piece
-    has_many :pieces through: :tags_piece
+    has_many :pieces, through: :tags_piece
 end
--- app/models/tags_piece.rb
+++ app/models/tags_piece.rb
@@ -1,2 +1,4 @@
 class TagsPiece < ApplicationRecord
+    belongs_to :piece
+    belongs_to :tag
 end
