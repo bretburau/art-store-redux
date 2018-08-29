0f9433327e29a7e4e8b3b2488b6d41e164206c2e
Bret <Burhooo@gmail.com>
2018-08-29 06:01:40 -0500

    Added PieceCollapse Component

--- client/src/components/Tag.js
+++ client/src/components/Tag.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 
 const Tag = (props) => {
+    const showPieces = () => {
+        
+    }
+    const piecesList = props.tag.pieces.map(piece => {
+        return <li>{piece.name}</li>
+    }) 
     return(
-        <li>{props.tag.name}</li>
+        <li>
+            <Button onClick={showPieces}>{props.tag.name}</Button>
+            <ul id={props.tag.id}>{piecesList}</ul>
+        </li>
     )
 }
 
