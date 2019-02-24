486cb1e69a0aef98f06d6ae058419e76e9c739b5
bretburau <github email address>
2019-02-24 11:50:17 -0600

    fixed reducer bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import { ConnectedApp } from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/root_reducer'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
 import AppRoutes from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk)),
+);
 // const store = createStore(rootReducer, applyMiddleware(thunk));
-const store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk));
+// const store = createStore(rootReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store} >
